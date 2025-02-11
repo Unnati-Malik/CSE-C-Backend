d757f87d13ade12b5002aa652cc06a649d6938f3
Unnati-Malik <unnatimalik7@gmail.com>
2025-02-11 10:11:01 +0530

    Commit

---
+++ CSE-C/FileSystem/filereadsync.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+// fs.writeFileSync("./data1.txt", "Hello ABES", "utf-8");
+// fs.writeFileSync("./data1.txt", "Hello ABESEC", "utf-8");
+// fs.appendFileSync("./data1.txt", " Students", "utf-8");
+// fs.renameSync("./data2.txt", "./data3.txt")
+// fs.unlinkSync("./data3.txt") //used for deletion of file, also ye rename command ke saath nahi chalegi
+
+
+const data = fs.readFileSync("./data.txt", "utf-8"); //this is a module to read file synchronously
+// console.log(data.toString());
+console.log(data);
+if(data.match("H")){
+    console.log("File Contains H");
+    const newdata = data.replace("H", "ABES");
+    fs.writeFileSync("./data.txt", newdata, "utf-8");
+
+}
